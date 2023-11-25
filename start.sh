ca4b4eddc07acfe070272daebd2c34f4c8c7b2a0
Jurgis Sakalauskas <jurgis.sakalauskas@cec.valantic.com>
2023-11-25 11:12:08 +0200

    some ts-node issues

--- start.sh
+++ start.sh
@@ -5,11 +5,12 @@ echo "Installing calibre"
 apt-get update -y
 apt-get install -y libopengl0
 apt-get install -y libegl1
+apt-get install -y libxcb-cursor0
 wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin
 
 # npm install
 echo "Installing dependencies"
-npm install
+npm install --include=dev
 
 if [ "$NODE_ENV" == "production" ]; then
     echo "Running prod"
